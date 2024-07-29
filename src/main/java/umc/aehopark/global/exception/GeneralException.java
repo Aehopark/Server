666bbae65aa4ca4146dd97a2f5cfa2d85ac74b79
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-07-29 23:40:40 +0900

    Feat: Handler 추가

--- src/main/java/umc/aehopark/global/exception/GeneralException.java
+++ src/main/java/umc/aehopark/global/exception/GeneralException.java
@@ -1,4 +1,21 @@
 package umc.aehopark.global.exception;
 
-public class GeneralException {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import umc.aehopark.global.common.dto.ErrorReasonDTO;
+import umc.aehopark.global.type.BaseErrorCode;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus() {
+        return this.code.getReasonHttpStatus();
+    }
 }
--- src/main/java/umc/aehopark/global/status/ErrorStatus.java
+++ src/main/java/umc/aehopark/global/status/ErrorStatus.java
@@ -11,6 +11,12 @@ import umc.aehopark.global.type.BaseErrorCode;
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
 
+    // 일반적인 응답
+    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러, 관리자에게 문의 바랍니다."),
+    _BAD_REQUEST(HttpStatus.BAD_REQUEST,"잘못된 요청입니다."),
+    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"인증이 필요합니다."),
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "금지된 요청입니다."),
+
     // User 관련 에러
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "사용자가 없습니다."),
     NICKNAME_INPUT_NOT_EXIST(HttpStatus.BAD_REQUEST, "닉네임을 입력해주세요"),
@@ -23,7 +29,7 @@ public enum ErrorStatus implements BaseErrorCode {
 
     private final HttpStatus httpStatus;
     private final String message;
-`
+
     @Override
     public ErrorReasonDTO getReason() {
         return ErrorReasonDTO.builder()
