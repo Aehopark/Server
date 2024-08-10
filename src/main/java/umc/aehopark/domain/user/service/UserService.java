ed0e3e067cd949a6f388652575a348835f37e884
GamerPLz <rbgur303@gmail.com>
2024-08-11 08:06:24 +0900

    fix: complete fix

--- src/main/java/umc/aehopark/domain/user/service/UserService.java
+++ src/main/java/umc/aehopark/domain/user/service/UserService.java
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
 package umc.aehopark.domain.user.service;
 
-import lombok.*;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-
-public class UserService {
-}
-
-
-=======
-package umc.aehopark.domain.user.service;
-
-import umc.aehopark.domain.user.dto.request.ReserveRequest;
 import umc.aehopark.domain.user.dto.request.AppleLoginRequest;
 import umc.aehopark.domain.user.dto.request.KakaoLoginRequest;
+import umc.aehopark.domain.user.dto.request.RegisterUserRequest;
+import umc.aehopark.domain.user.dto.request.ReserveRequest;
 import umc.aehopark.domain.user.dto.request.UserImageAlterRequest;
 import umc.aehopark.domain.user.dto.request.UserNameAlterRequest;
 import umc.aehopark.domain.user.dto.request.isMarketingRequest;
@@ -24,18 +11,26 @@ import umc.aehopark.domain.user.dto.response.AppleLoginResponse;
 import umc.aehopark.domain.user.dto.response.InfoUserResponse;
 import umc.aehopark.domain.user.dto.response.KakaoLoginResponse;
 import umc.aehopark.domain.user.dto.response.RegisterUserResponse;
-import umc.aehopark.domain.user.dto.request.RegisterUserRequest;
 
 public interface UserService {
-    void reserve(ReserveRequest reserveRequest);
-    Boolean checkNick(String nickname);
-    RegisterUserResponse register(RegisterUserRequest registerRequest);
-    KakaoLoginResponse kakaoLogin(KakaoLoginRequest kakaoLoginRequest);
-    AppleLoginResponse appleLogin(AppleLoginRequest appleLoginRequest);
-    InfoUserResponse infoUser(Long userId);
-    void delete(Long userId);
-    void alterName(Long userId, UserNameAlterRequest nickname);
-    void alterUserImage(Long userId, UserImageAlterRequest imageUrl);
-    void isMarketing(Long userId, isMarketingRequest isMarketing);
+	void reserve(ReserveRequest reserveRequest);
+
+	Boolean checkNick(String nickname);
+
+	RegisterUserResponse register(RegisterUserRequest registerRequest);
+
+	KakaoLoginResponse kakaoLogin(KakaoLoginRequest kakaoLoginRequest);
+
+	AppleLoginResponse appleLogin(AppleLoginRequest appleLoginRequest);
+
+	InfoUserResponse infoUser(Long userId);
+
+	void delete(Long userId);
+
+	void alterName(Long userId, UserNameAlterRequest nickname);
+
+	void alterUserImage(Long userId, UserImageAlterRequest imageUrl);
+
+	void isMarketing(Long userId, isMarketingRequest isMarketing);
 }
->>>>>>> c87bb972cc4558dad03ba25f39de87f259b87e38
+
